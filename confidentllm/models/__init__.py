7907784deae6a789a56bfc41cb286f275f9ac94b
Carel van Niekerk <cvanniekerk@hhu.de>
2024-07-10 15:03:20 +0200

    feat: Add load_model function to models/__init__.py

--- confidentllm/models/__init__.py
+++ confidentllm/models/__init__.py
@@ -1,12 +1,33 @@
 from hydra_zen import builds
+from torch.nn import Module
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+
+def load_model(pretrained_model_name_or_path: str, device: str) -> Module:
+    """Load a pretrained model from Hugging Face's model hub.
+
+    Args:
+    ----
+        pretrained_model_name_or_path (str): The name of or the path to the model.
+        device (str): The device to load the model on.
+
+    Returns:
+    -------
+        Module: The model loaded on the specified device.
+
+    """
+    model: Module = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path)
+
+    return model.to(device)
+
+
 CausalLMConfig = builds(
-    AutoModelForCausalLM.from_pretrained,
-    pretrained_model_name_or_path="gpt2",
+    load_model,
+    pretrained_model_name_or_path="google/gemma-1.1-2b-it",
+    device="cuda",
 )
 
 TokenizerConfig = builds(
     AutoTokenizer.from_pretrained,
-    pretrained_model_name_or_path="gpt2",
+    pretrained_model_name_or_path="google/gemma-1.1-2b-it",
 )
