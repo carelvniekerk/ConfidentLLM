b84910e19ff1428862760ff051b08d876b93e2f2
Carel van Niekerk <cvanniekerk@hhu.de>
2024-07-11 14:13:13 +0200

    Formatting

--- confidentllm/models/__init__.py
+++ confidentllm/models/__init__.py
@@ -1,8 +1,29 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------
+# Project: Training Project
+# Author: Carel van Niekerk
+# Year: 2024
+# Group: Dialogue Systems and Machine Learning Group
+# Institution: Heinrich Heine University Düsseldorf
+# --------------------------------------------------------------------------------
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License."
+"""Module to load pretrained models and tokenizers from Hugging Face's model hub."""
+
 from enum import StrEnum
 
 import torch
 from hydra_zen import make_custom_builds_fn, store
-from omegaconf import OmegaConf
 from torch.nn import Module
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer
 
