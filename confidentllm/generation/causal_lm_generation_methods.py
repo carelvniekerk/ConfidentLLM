2448133ae3098c429cc9198771a65d689b69dfde
Carel van Niekerk <cvanniekerk@hhu.de>
2024-08-29 17:38:06 +0200

    Added disparity as the confidence score to greedy decoding

--- confidentllm/generation/causal_lm_generation_methods.py
+++ confidentllm/generation/causal_lm_generation_methods.py
@@ -85,9 +85,25 @@ class GreedyCausalLMGenerationMethod(CausalLMGenerationMethod):
         ).unsqueeze(0)
         generation_scores = torch.softmax(generation_scores, dim=-1)
 
+        # TODO: Modularise this confidence extration function
+        next_tokens: torch.Tensor = generation_output.sequences[
+            :,
+            -generation_scores.size(-2) :,
+        ]
+        next_token_prob: torch.Tensor = generation_scores[0][
+            range(next_tokens.size(1)),
+            next_tokens[0],
+        ].unsqueeze(0)
+        second_highest_prob: torch.Tensor = torch.topk(
+            generation_scores[0],
+            k=2,
+            dim=-1,
+        )[0][:, 1].unsqueeze(0)
+        disparity: torch.Tensor = next_token_prob - second_highest_prob
+
         return GenerationOutput(
             generated_ids=generation_output.sequences,
-            generation_scores=generation_scores,
+            generation_scores=disparity,
         )
 
 
