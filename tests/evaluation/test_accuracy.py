9118e29421f8139b8171d655d4f8a72959b71f96
Carel van Niekerk <cvanniekerk@hhu.de>
2024-09-09 11:17:03 +0200

    Refactor pytest configuration and add test cases for accuracy evaluation

--- tests/evaluation/test_accuracy.py
+++ tests/evaluation/test_accuracy.py
@@ -25,7 +25,63 @@
 
 import pytest
 
+from confidentllm.evaluation import EvaluationBatch
 from confidentllm.evaluation.accuracy import AccuracyEvaluator, AccuracyResults
 
 
-# TODO: Add more tests
+@pytest.fixture
+def test_data() -> list[EvaluationBatch]:
+    """Create a test dataset."""
+    return [
+        EvaluationBatch(
+            labels=[0, 1, 2],
+            predictions=[0, 1, 2],
+        ),
+        EvaluationBatch(
+            labels=[-1, 3, 4],
+            predictions=[0, 1, 1],
+        ),
+    ]
+
+
+@pytest.fixture
+def accuracy_evaluator() -> AccuracyEvaluator:
+    """Create an accuracy evaluator."""
+    return AccuracyEvaluator(padding_value=-1)
+
+
+def test_adding_batches(
+    test_data: list[EvaluationBatch],
+    accuracy_evaluator: AccuracyEvaluator,
+) -> None:
+    """Test adding batches to the accuracy evaluator."""
+    for batch in test_data:
+        accuracy_evaluator.add_batch(batch)
+
+    if accuracy_evaluator.buffer.labels != [0, 1, 2, -1, 3, 4]:
+        msg = (
+            f"The labels were not stored correctly, expected {[0, 1, 2, -1, 3, 4]},"
+            f" got {accuracy_evaluator.buffer.labels}"
+        )
+        raise AssertionError(msg)
+    if accuracy_evaluator.buffer.predictions != [0, 1, 2, 0, 1, 1]:
+        msg = (
+            f"The predictions were not stored correctly, expected {[0, 1, 2, 0, 1, 1]},"
+            f" got {accuracy_evaluator.buffer.labels}"
+        )
+        raise AssertionError(msg)
+
+
+def test_accuracy(
+    test_data: list[EvaluationBatch],
+    accuracy_evaluator: AccuracyEvaluator,
+) -> None:
+    """Test the accuracy calculation."""
+    for batch in test_data:
+        accuracy_evaluator.add_batch(batch)
+
+    results: AccuracyResults = accuracy_evaluator.evaluate()
+
+    if results.accuracy != 3 * 100 / 5:
+        msg = f"Expected accuracy to be {3 * 100 / 5}, got {results.accuracy}"
+        raise AssertionError(msg)
