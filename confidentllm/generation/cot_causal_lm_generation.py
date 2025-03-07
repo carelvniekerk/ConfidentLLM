89b4a4ee91b4c19f3936764c6d0d5b169f854c02
Carel van Niekerk <vniekerk.carel@gmail.com>
2025-03-07 11:38:56 +0100

    Add padding strategy and max length truncation to beam and CoT generation methods

--- confidentllm/generation/cot_causal_lm_generation.py
+++ confidentllm/generation/cot_causal_lm_generation.py
@@ -29,6 +29,7 @@ import torch
 from hydra_zen import store
 from transformers import BatchEncoding, TensorType
 from transformers.generation import GenerateDecoderOnlyOutput
+from transformers.tokenization_utils_base import PaddingStrategy
 
 from confidentllm.conversations.create_chat import create_conversation
 from confidentllm.generation.types import (
@@ -72,11 +73,15 @@ class CoTDecodingCausalLMGenerationMethod(CausalLMGenerationMethod):
         if responses is None:
             conversation.messages[0] = conversation.messages[0][:-1]
 
+        context_max_length: int = self.tokenizer.model_max_length - self.max_length
         inputs: BatchEncoding = self.tokenizer.apply_chat_template(
             list(conversation),
             add_generation_prompt=True,
             return_tensors=TensorType.PYTORCH,
             return_dict=True,
+            padding=PaddingStrategy.MAX_LENGTH,
+            truncation=True,
+            max_length=context_max_length,
         )  # type: ignore[reportAssignmentType] # When using return dict a type BatchEncoding is returned
         inputs = inputs.to(self.model.device)
 
