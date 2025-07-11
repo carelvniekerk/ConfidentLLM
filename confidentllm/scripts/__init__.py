5266b05f9665a553845e2bda4e213172d566a25f
Carel van Niekerk <vniekerk.carel@gmail.com>
2025-07-11 14:14:10 +0200

    Remove quantile regression evaluation script references from project files

--- confidentllm/scripts/__init__.py
+++ confidentllm/scripts/__init__.py
@@ -24,9 +24,6 @@
 """Execcution scripts for different tasks."""
 
 from confidentllm.scripts.run_download_and_sync import main as download_and_sync
-from confidentllm.scripts.run_quantile_regression_evaluation import (
-    main as quantile_regression_evaluation,
-)
 from confidentllm.scripts.run_question_answering import main as question_answering
 from confidentllm.scripts.run_reinforcement_learning import (
     main as reinforcement_learning,
@@ -38,7 +35,6 @@ from confidentllm.scripts.run_training import main as train
 
 __all__ = [
     "download_and_sync",
-    "quantile_regression_evaluation",
     "question_answering",
     "reinforcement_learning",
     "reward_model_evaluation",
